afd4c16c556d39a199031da4ef1792bc9a152f85
Bry Cox <carl.cc@gmail.com>
2019-07-12 15:50:37 +0000

    Added app/controller/application_controller, db, Rakefile.

---
+++ app/controller/application_controller.rb
@@ -0,0 +1,18 @@
+class ApplicationController < Sinatra::Base
+
+  enable :sessions
+  use Rack::Flash
+
+  configure do
+    set :session_secret, "glyyph"
+    set :public_folder, 'public'
+    set :views, 'app/views'
+  end
+
+  get '/' do
+
+    erb :index
+
+  end
+
+end
