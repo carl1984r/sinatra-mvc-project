531fa3a7b15940876f553933acb3e9ae97a6be84
Bry Cox <carl.cc@gmail.com>
2019-07-15 14:21:39 +0000

    modified:   db/development.sqlite         modified:   db/seeds.rb

---
+++ db/seeds.rb
@@ -0,0 +1,44 @@
+
+user_table = ["newusername", "bathsalt@hotmail.com", "encryptopass"],
+             ["couchmuffin", "grapes@mail.com", "cryptopass"],
+             ["fodder", "7878bathsalt@hotmail.dot", "yptopass"],
+             ["orangecabbage", "salt@coldmail.com", "enryptopas"],
+             ["nilthought", "yellow@nail.com", "cryptop"]
+
+user_table.each do |x|
+  u = User.new
+  u.username = x[0]
+  u.email = x[1]
+  u.password = x[2]
+  u.save
+end
+
+airports_table = ["ECP", "PCB International"],
+             ["F95", "Altha Priv."],
+             ["AAF", "Apalachicola"],
+             ["ORL", "Orlando"],
+             ["0J6", "Headland"]
+
+airports_table.each do |x|
+  u = Airport.new
+  u.airport_code = x[0]
+  u.airport_name = x[1]
+  u.save
+end
+
+reviews_table = ["Amazing experience!"],
+             ["Excellent distance to food!"],
+             ["Great to visit old Florida."],
+             ["Mickey Mouse!"],
+             ["Country airport."]
+
+reviews_table.each do |x|
+  u = Review.new
+  u.content = x[0]
+  u.save
+end
+
+User.first.airports << Airport.last
+Airport.first.reviews << Review.last
+User.last.airports << Airport.first
+Airport.last.reviews << Review.first
