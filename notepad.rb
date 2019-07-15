46037f38fa7afe697c0248d736d3f2a09deece51
Bry Cox <carl.cc@gmail.com>
2019-07-15 01:19:47 +0000

    modified:   db/development.sqlite         new file:   db/seeds.rb         modified:   notepad.rb

--- notepad.rb
+++ notepad.rb
@@ -1,1 +1,3 @@
 #Airport.create(:airport_code => "0J6", :airport_name=> "Headland International")
+#User.create(:username => "newcontent", :email => "nono@email.com", :password_digest => "email.com")
+#Review.create(:content => "Elysian Cough")
