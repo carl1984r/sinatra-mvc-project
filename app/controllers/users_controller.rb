0160100646024421c4fe8e52e965a96e74c2e289
Bry Cox <carl.cc@gmail.com>
2019-07-16 00:08:22 +0000

    modified:   app/controllers/users_controller.rb         deleted:    app/views/airports.erb         new file:   app/views/done.erb         modified:   app/views/index.erb         modified:   app/views/layout.erb         new file:   app/views/users/signup.erb         modified:   db/development.sqlite

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,19 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
+
+    erb :'users/signup'
+
+  end
+
+  post '/signup' do
+
+    user = User.create(:username => params["username"], :email => params["email"], :password => params["password"])
+    session[:user_id] = user.id
+
+    erb :done
+
+  end
+
+
 end
