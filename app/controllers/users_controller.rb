86d5f3d4d2ceb7fdd8ded6bca0b99ba54b622a01
Bry Cox <carl.cc@gmail.com>
2019-07-16 20:24:40 +0000

    modified:   app/controllers/users_controller.rb         new file:   app/helpers/helpers.rb         modified:   app/views/done.erb         new file:   app/views/users/login.erb         modified:   app/views/users/signup.erb         modified:   db/development.sqlite

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,10 +8,44 @@ class UsersController < ApplicationController
 
   post '/signup' do
 
-    user = User.create(:username => params["username"], :email => params["email"], :password => params["password"])
-    session[:user_id] = user.id
+    if  params[:username].empty? || params[:email].empty? || params[:password].empty?
+        flash[:empty_signup_form_error] = "Complete all form fields."
+        erb :'users/signup'
+    else
+        user = User.create(:username => params["username"], :email => params["email"], :password => params["password"])
+        session[:user_id] = user.id
 
-    erb :done
+        erb :done
+    end
+  end
+
+  get '/login' do
+
+    erb :'/users/login'
+
+  end
+
+  post '/login' do
+
+    user = User.find_by(:username => params["username"])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      erb :done
+    else
+      flash[:login_error] = "Incorrect login. Try again?"
+      erb :'/users/login'
+    end
+  end
+
+  get '/logout' do
+
+    if Helpers.is_logged_in?(session)
+      session.clear
+      flash[:logged_out] = "You are now logged out."
+      erb :'/users/login'
+    else
+      erb :index
+    end
 
   end
 
