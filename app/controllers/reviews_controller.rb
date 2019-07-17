af03cba77e50bb078067d1547b5968d2339f99b3
Bry Cox <carl.cc@gmail.com>
2019-07-17 13:27:19 +0000

    modified:   app/controllers/reviews_controller.rb

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,3 +1,16 @@
 class ReviewsController < ApplicationController
 
+  get '/reviews' do
+
+    if !Helpers.is_logged_in?(session)
+      redirect to '/login'
+    end
+
+    @reviews = Review.all
+    @user = Helpers.current_user(session)
+
+    erb :'/reviews/reviews'
+
+  end
+
 end
