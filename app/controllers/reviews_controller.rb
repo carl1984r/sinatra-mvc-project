36c0a3edc680ddf2bb0f2ed2883d115391e74e6c
Bry Cox <carl.cc@gmail.com>
2019-07-17 13:58:47 +0000

    modified:   app/controllers/reviews_controller.rb         modified:   app/views/users/login.erb

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,6 +3,7 @@ class ReviewsController < ApplicationController
   get '/reviews' do
 
     if !Helpers.is_logged_in?(session)
+      flash[:please_login] = "Please login to view content."
       redirect to '/login'
     end
 
@@ -10,6 +11,18 @@ class ReviewsController < ApplicationController
     @user = Helpers.current_user(session)
 
     erb :'/reviews/reviews'
+
+  end
+
+  get '/reviews/:id' do
+
+    if !Helpers.is_logged_in?(session)
+      flash[:please_login] = "Please login to view content."
+      erb :'/users/login'
+    end
+
+    @review = Review.find(params[:id])
+    erb :"reviews/show"
 
   end
 
