53ef8e4c05fec2577fac92f07eb3630267876d1f
Bry Cox <carl.cc@gmail.com>
2019-07-18 14:34:01 +0000

    modified:   app/helpers/helpers.rb         deleted:    app/views/done.erb         modified:   app/views/reviews/edit.erb         modified:   app/views/reviews/new.erb         modified:   app/views/reviews/reviews.erb         modified:   app/views/users/login.erb         modified:   app/views/users/signup.erb         modified:   db/development.sqlite

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -8,4 +8,8 @@ class Helpers
     !!session_hash[:user_id]
   end
 
+  def self.flash_types
+    [:empty_signup_form_error, :login_error, :logged_out, :empty_content_error, :review_created, :please_login, :login_to_edit, :wrong_user_edit, :no_content_edit, :delete_error, :wrong_user_delete, :success, :create_error]
+  end
+
 end
