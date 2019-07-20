dd57dfbd06e3a6cda74476d3b69bca57e09e237a
Bry Cox <carl.cc@gmail.com>
2019-07-20 21:38:25 +0000

    modified:   app/controllers/airports_controller.rb         modified:   app/views/airports/airports.erb         new file:   app/views/airports/show.erb         modified:   app/views/reviews/reviews.erb         modified:   db/development.sqlite

--- app/controllers/airports_controller.rb
+++ app/controllers/airports_controller.rb
@@ -11,10 +11,27 @@ class AirportsController < ApplicationController
     else
 
       @airports = User.find_by(:id => session[:user_id]).airports
+      @user = Helpers.current_user(session)
       erb :"/airports/airports"
 
     end
 
+  end
+
+  get '/airports/:id' do
+
+    if !Helpers.is_logged_in?(session)
+
+      flash[:please_login] = "Please login to view content."
+      erb :'/users/login'
+
+    else
+
+      @reviews = Airport.find(params[:id]).reviews
+      @user = Helpers.current_user(session)
+      erb :"airports/show"
+
+    end
 
   end
 
